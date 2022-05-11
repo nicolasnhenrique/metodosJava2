b4abf6e1c628073855d4ec862f8d27f7cc5c2949
nicolasnnshenrique <nicolasnnnshenrique@gmail.com>
2022-05-11 14:39:09 -0300

    Initial commit

---
+++ src/dioBootcamp/Main.java
@@ -0,0 +1,15 @@
+package dioBootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(8,7,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
+
